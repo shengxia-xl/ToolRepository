e532bc5d723eb02b3563c4dd9635335ebba13db5
shengxia-xl <shengxia_xl@163.com>
2018-07-02 00:12:23 +0800

    [xuli]:更新
    增加 Mystrcat.

--- C++/String/MyString.cpp
+++ C++/String/MyString.cpp
@@ -42,6 +42,46 @@ size_t Mystrlen(const char* str)
     return str_len;    
 }
 
+/*
+    字符串src附加到dest后，返回的是dest
+    
+使用一个临时的指针，用循环来指向dest的末尾，再把src中的指针接续
+
+while (*cp++ = *src++)
+等价于
+while(*cp)
+{
+    *cp = *src;
+    cp++;
+    src++;
+}
+
+解析：
+while( *cp++ = *src++ )
+首先*cp = *src 赋值
+然后判断表达式的值while(*cp)
+然后自增cp++, src++
+所以，整个表达式的结束判断就在于while(*cp)
+如果*cp=='\0'，因为'\0'对应的ascii码就是0，所以判断就为假，也就退出循环了
+    
+*/
+char* Mystrcat(char* dest, const char* src)
+{
+    assert((dest != NULL) && (src != NULL));
+    printf("%d\n", __LINE__);
+    
+    char* cp = dest;
+    while (*cp) {
+        cp++;
+    }
+    
+    while (*cp++ = *src++) {
+    }
+    
+    return dest;
+}
+
+
 int main() {
     
     string str = "abc";
@@ -50,6 +90,8 @@ int main() {
     char astr[] = "123";
     char* pstr = "abc123";//warning: deprecated conversion from string constant to ‘char*’ [-Wwrite-strings]
     const char* pstr_c = "abc123";// 不会有警告
+    char tchar[3] = {'1', '2', '3'};// 123xuli 不会发生数组越界
+    char* strt = "xuli";
 
     cout<< str << endl;// abc
     // string不是c语言的内置数据 printf无法直接打印string  需要转换为char*
@@ -68,6 +110,9 @@ int main() {
     
     printf("%s\n", pstr);// abc123
     printf("%zu",Mystrlen(pstr));// 6
+
+    char* temp = Mystrcat(tchar, strt);
+    printf("%s", temp);// 123xuli
 
     return 0;
 }
