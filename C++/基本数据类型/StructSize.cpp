7f670da699f116be91435c8f0cebc185d0246eff
xuli <shengxia_xl@163.com>
2018-07-09 15:08:17 +0800

    [xuli]:更新 1、代码 增加计算结构体包含联合体，数组，其他结构体的大小计算 2、知识点总结 增加计算方法

--- C++/基本数据类型/StructSize.cpp
+++ C++/基本数据类型/StructSize.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+struct A;
+
+// 联合体大小就是成员中最大类型的大小
+union C{
+	char ch1;
+	int j;
+};
+
 /*
  1 2 4 = 8
  1 4 2 = 12
@@ -13,11 +21,27 @@ using namespace std;
  4 2 1 = 8
 */
 // struct 没有任何类型时，为1
-// static 类型不会计算在对象中 
+// static 类型不会计算在对象大小中 
+
 struct A {
-//	char a1;
+//	static char a1;
 //	int fun(); 
+	char a1;
+	short a3;
+	int a2;
+	char a4[10];
 };
+
+// 成员包含结构体的结构体
+// 把结构体拆开来计算
+// 结构体A 拆成 int char short 4 1 2  
+struct B 
+{
+	A b2; // 1 4 2 = 12 
+	int b1;
+	short b3;
+}; 
+
 
 int main ()
 {
@@ -37,9 +61,9 @@ int main ()
 //	printf("sizeof(double) = %d\n", sizeof(double));// 8
 //	printf("sizeof(long double) = %d\n", sizeof(long double));// 16
 //	printf("sizeof(double long) = %d\n", sizeof(double long));// 16
-	
-	
-	printf("%d", sizeof(A));
+
+	printf("%d\n", sizeof(A));
+	printf("%d\n", sizeof(B));
 	return 0; 
 }
 
