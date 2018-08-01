a469e5c16005137d62417f7a594f8154ad74b7ff
xuli <shengxia_xl@163.com>
2018-08-01 20:13:11 +0800

    [xuli]: 更新 1、基本数据类型 [文档] 	类的大小的计算         当有继承时，类的大小计算 虚继承 多继承等情况 2、ClassSize [代码] 	有继承时，类的大小计算

---
+++ C++/基本数据类型/ClassSize.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class B     
+{  
+    char ch;   
+	int a;  
+//    virtual void func0()  {  }   
+};   
+
+class C    
+{  
+    char ch1;  
+    char ch2;  
+    int b;
+//    virtual void func()  {  }    
+//    virtual void func1()  {  }   
+};
+
+class E: public B, public C  
+{     
+    int e;     
+//    virtual void func0()  {  }   
+//    virtual void func1()  {  }  
+}; 
+
+
+int main()
+{
+	cout<< sizeof(B) <<endl; //  16
+	cout<< sizeof(C) <<endl; //  16
+	cout<< sizeof(E) <<endl; // 32
+
+	return 0;	
+}
