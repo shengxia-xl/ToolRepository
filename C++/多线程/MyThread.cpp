3ddf41a45ece220981b5729d8fd1018b1d2c28e8
xuli <shengxia_xl@163.com>
2018-08-08 18:06:19 +0800

    [xuli]: 更新 1、[总览] 	QT中的信号槽 事件 QObject 2、[多线程] 文档 	线程间通信  信号量

--- C++/多线程/MyThread.cpp
+++ C++/多线程/MyThread.cpp
@@ -1,15 +1,48 @@
-
-
 #include <iostream>
 #include <stdio.h>
 #include <Windows.h>
 #include <thread>
-
+#include <Mutex>
+#include <condition_variable>
 
 using namespace std;
+mutex mut;
+condition_variable cv;
+bool flag = false;
+
+void printfA()
+{
+	unique_lock<mutex> ulck(mut);
+	for(int i = 0; i < 10; i++) {
+		Sleep(1000);
+		cv.wait(ulck, [] {return flag; });
+		cout<<std::this_thread::get_id()<<endl;
+		cout<<"A"<<endl;
+		flag = false;
+		cv.notify_one();
+	}
+}
+
+void printfB()
+{
+	unique_lock<mutex> ulck(mut);
+	for(int i = 0; i < 10; i++) {
+		Sleep(1000);
+		cv.wait(ulck, []{return !flag; });
+		cout<< this_thread::get_id()<<endl;
+		cout<<"B"<<endl;
+		flag = true;
+		cv.notify_one();
+	}
+}
 
 int main() 
 {
+	thread th1(printfA);
+	thread th2(printfB);
+	th1.join();
+	th2.join();
+	
 	system("pause");
 	return 0;
 }
